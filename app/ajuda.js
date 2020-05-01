0aeed7226983f391d83b9a6a6677d35c7c3a2d62
Carlos Melo <carlos.melo.contact@gmail.com>
2020-05-01 14:33:50 -0300

    add estado a relação ajuda

--- app/ajuda.js
+++ app/ajuda.js
@@ -4,6 +4,7 @@ const mongoose = restful.mongoose
 const ajudaSchema = new mongoose.Schema({
     nome: {type: String, required: true},
     contato: {type: String, required: true},
+    estado: {type: String, required: true},
     cidade: {type: String, required: true},
     logradouro: {type: String, required: false},
     descricao: {type: String, required: true},
