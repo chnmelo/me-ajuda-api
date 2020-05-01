302be6ad058f0944a5e0bfed63a373ab31f1745e
Carlos Melo <carlos.melo.contact@gmail.com>
2020-05-01 15:26:06 -0300

    rotas de filtro por cidade e estado

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
 module.exports = mongoose.connect(process.env.MONGO_URL,{
+// module.exports = mongoose.connect("mongodb://localhost/dbMeAjuda",{
     useNewUrlParser: true
 })
--- config/routes.js
+++ config/routes.js
@@ -16,6 +16,28 @@ module.exports = function(server){
     const ajudaService = require('../app/ajudaService')
     ajudaService.register(router,'/ajuda')
 
+    router.get('/ajuda/:id', function (req, res) {
+        ajudaService.finByID(req.params.id)
+            .then( subscriber => {
+                res.locals.subscriber = subscriber;
+                next();
+            } )
+            .catch( error => {
+                console.log( `Error fetching subscriber by ID: ${error.message}` );
+                next( error );
+            } );
+    });
+    router.get('/ajuda/cidade/:cidade', function (req, res) {
+        ajudaService.find({ cidade: req.params.cidade}, function(err, data){
+            res.json(data);
+        })
+    });
+    router.get('/ajuda/estado/:estado', function (req, res) {
+        ajudaService.find({ estado: req.params.estado}, function(err, data){
+            res.json(data);
+        })
+    });
+
     const ajudoService = require('../app/ajudoService')
     ajudoService.register(router,'/ajudo')
 }
